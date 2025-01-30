de3e10030ceaf730c0e77a3a8ef91d6f187ee58f
Imil <ibragimovimil0@gmail.com>
2025-01-30 22:11:58 +0300

    [feat] Add gracefull shutdown

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("Hello")
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	serveMux := &http.ServeMux{}
+
+	server := &http.Server{
+		Addr:    "localhost:5050",
+		Handler: serveMux,
+	}
+
+	go func() {
+		log.Println("Server started listening")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("Server stopped listening")
 }
