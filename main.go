869d26910257252154a401be4662b69155303ad9
Imil <ibragimovimil0@gmail.com>
2025-02-01 20:40:32 +0300

    [feat] Add PostgreSQL connection

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
+
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Database: %s\n", err)
+	}
+	defer db.Close()
 
 	serveMux := &http.ServeMux{}
 	serveMux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
