ac2639f2b218c0cb5e1616352a7cf8175be2557a
Imil <ibragimovimil0@gmail.com>
2025-02-05 19:13:17 +0300

    [refactor] Update Product struct

--- domain/product.go
+++ domain/product.go
@@ -1,18 +1,39 @@
 package domain
 
 type Product struct {
-	id   int
-	name string
+	id         int
+	sellerId   int
+	categoryId int
+	name       string
+	price      float32
 }
 
-func NewProduct(id int, name string) Product {
-	return Product{id: id, name: name}
+func NewProduct(id int, name string, sellerId int, categoryId int, price float32) Product {
+	return Product{
+		id:         id,
+		name:       name,
+		sellerId:   sellerId,
+		categoryId: categoryId,
+		price:      price,
+	}
 }
 
-func (p Product) GetID() int {
+func (p Product) ID() int {
 	return p.id
 }
 
-func (p Product) GetName() string {
+func (p Product) Name() string {
 	return p.name
+}
+
+func (p Product) SellerID() int {
+	return p.sellerId
+}
+
+func (p Product) CategoryID() int {
+	return p.categoryId
+}
+
+func (p Product) Price() int {
+	return p.categoryId
 }
