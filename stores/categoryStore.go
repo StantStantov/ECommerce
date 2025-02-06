e7fc4da28655d9ddff9f759b37767f3e97ffdca6
Imil <ibragimovimil0@gmail.com>
2025-02-06 21:06:40 +0300

    [refactor] Let CategoryStore use Category

--- stores/categoryStore.go
+++ stores/categoryStore.go
@@ -1,6 +1,7 @@
 package stores
 
 import (
+	"Stant/ECommerce/domain"
 	"database/sql"
 	"fmt"
 )
@@ -13,31 +14,33 @@ func NewCategoryStore(db *sql.DB) *CategoryStore {
 	return &CategoryStore{db: db}
 }
 
-func (st CategoryStore) Read(id int) (string, error) {
+func (st CategoryStore) Read(categoryID int) (domain.Category, error) {
 	q := "SELECT * FROM categories WHERE category_id = $1"
-	row := st.db.QueryRow(q, id)
-	var category string
-	if err := row.Scan(category); err != nil {
-		return "", fmt.Errorf("SQL Read: %v", err)
+	row := st.db.QueryRow(q, categoryID)
+	var id int
+	var name string
+	if err := row.Scan(&id, &name); err != nil {
+		return domain.Category{}, fmt.Errorf("SQL Read: %v", err)
 	}
-	return category, nil
+  return domain.NewCategory(id, name), nil
 }
 
-func (st CategoryStore) ReadAll() ([]string, error) {
+func (st CategoryStore) ReadAll() ([]domain.Category, error) {
 	q := "SELECT * FROM products"
 	rows, err := st.db.Query(q)
 	if err != nil {
 		return nil, err
 	}
 
-	categories := []string{}
+	categories := []domain.Category{}
 	defer rows.Close()
 	for rows.Next() {
-		var category string
-		if err := rows.Scan(category); err != nil {
+		var id int
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
 			return nil, fmt.Errorf("SQL ReadAll: %v", err)
 		}
-		categories = append(categories, category)
+		categories = append(categories, domain.NewCategory(id, name))
 	}
 	return categories, nil
 }
