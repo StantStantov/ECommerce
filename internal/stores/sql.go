e19ec2162dd592e0c5de6447631a2daf096e3bcf
Imil <ibragimovimil0@gmail.com>
2025-03-21 14:31:14 +0300

    [feat] Make NewDBConn checn env vars and db health

--- internal/stores/sql.go
+++ internal/stores/sql.go
@@ -2,21 +2,44 @@ package stores
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 )
 
 func NewDBConn() (*sql.DB, error) {
-	pgConn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DB"),
-	)
+	user, ok := os.LookupEnv("POSTGRES_USER")
+	if !ok || len(user) == 0 {
+		return nil, errors.New("stores.NewDBConn: [Env POSTGRES_USER doesn't exist or empty]")
+	}
+	password, ok := os.LookupEnv("POSTGRES_PASSWORD")
+	if !ok || len(password) == 0 {
+		return nil, errors.New("stores.NewDBConn: [Env POSTGRES_PASSWORD doesn't exist or empty]")
+	}
+	host, ok := os.LookupEnv("POSTGRES_HOST")
+	if !ok || len(host) == 0 {
+		return nil, errors.New("stores.NewDBConn: [Env POSTGRES_HOST doesn't exist or empty]")
+	}
+	port, ok := os.LookupEnv("POSTGRES_PORT")
+	if !ok || len(port) == 0 {
+		return nil, errors.New("stores.NewDBConn: [Env POSTGRES_PORT doesn't exist or empty]")
+	}
+	dbname, ok := os.LookupEnv("POSTGRES_DB")
+	if !ok || len(dbname) == 0 {
+		return nil, errors.New("stores.NewDBConn: [Env POSTGRES_DB doesn't exist or empty]")
+	}
 
+	pgConn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+		user, password, host, port, dbname,
+	)
 	db, err := sql.Open("pgx", pgConn)
-	return db, err
+	if err != nil {
+		return nil, fmt.Errorf("stores.NewDBConn: [%w]", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("stores.NewDBConn: [%w]", err)
+	}
+	return db, nil
 }
 
 type sqlRow interface {
